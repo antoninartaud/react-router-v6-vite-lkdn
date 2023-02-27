73d25ec0c7844aa915cf0d5d87947472c492e015
Antonin Artaud <antoninartaud@hotmail.fr>
2023-02-27 11:28:14 +0100

    feat: home & categories routes initialization

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './components/App';
+import { BrowserRouter as Router } from 'react-router-dom';
+
 import './index.css';
+import App from './components/App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </React.StrictMode>
 );
